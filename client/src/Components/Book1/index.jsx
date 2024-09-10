d2bfff37fabf0f1dcc54d6f076c8236f7530e6e7
stephanie jones <sjones.njones@gmail.com>
2024-09-09 19:16:23 -0500

    finished book page style

--- client/src/Components/Book1/index.jsx
+++ client/src/Components/Book1/index.jsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row, Image, Button } from "react-bootstrap";
+import { Col, Row, Image, Button } from "react-bootstrap";
 
 import Book1Pic from '../../assets/images/book1.png';
 import '../../App.css';
@@ -6,22 +6,24 @@ import '../../App.css';
 const Book1 = () => {
   return (
     <div className=''>
-      <h1 className='lifeSavers fw-bolder pb-4 text-white'> The Whimsical Journey of Ari and Her Amazing AFOs - Book 1</h1>
-           <Row className="mx-5 px-5">
-        <Col>
-          <Image className='w-100
+      <h1 className='lifeSavers fw-bolder bookTitle pb-4 text-white'> The Whimsical Journey of Ari and Her Amazing AFOs - Book 1</h1>
+      <Row className='mx-auto bookRow' xs={1} md={1} lg={2}>
+        <Col className="image">
+          <Image className='bookPic 
          ' src={Book1Pic} fluid />;
         </Col>
         <Col>
-          <div className=''>
-            <h3 className='openSans text-center w-50 text-white'>
+            <p className='bookText mx-auto workSans'>
             Shall we go on an adventure together? Join Ari-Elizabeth on her whimsical journey of discover as she learns that her AFOs (ankle-foot orthoses) aren't a limitation, but a gift to help others. 
-            </h3>
-          </div>
-          <div className='mx-auto'>
+            </p>
+            <div>
+              
 
-         <a className='' href="https://a.co/d/iC3YyYf">
-         <Button className="button-62 text-white fs-5">Order From Amazon</Button>
+          <div className='text-center pt-5 pb-2'>
+<small className=' text-white workSans fs-4'>$19.99</small>
+              </div>              
+         <a className='bookButtonPlacement' href="https://a.co/d/iC3YyYf">
+         <Button className="lifeSavers bookButtonPlacement btn-hover">Order From Amazon</Button>
                  </a> 
           </div>
              
@@ -31,6 +33,8 @@ const Book1 = () => {
     </div>
   );
 };
+
+
 
 export default Book1;
 
